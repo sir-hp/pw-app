8a3d14ad1c10648462db14ab845e87a7a69bb89d
sir-hp <77618213+sir-hp@users.noreply.github.com>
2022-01-12 11:14:41 +0100

    spagetthi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OneSignal from 'react-onesignal'
 
 function App() {
+  
+  useEffect(() => {
+    OneSignal.init({
+      appId: "28e4011c-9ebc-458a-a6e5-0c017a73d389",
+    });
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
